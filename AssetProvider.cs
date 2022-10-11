030c75343f6c59ff5e553cb3241b98a0db1f68ac
Lekret <lekret399@mail.ru>
2022-10-11 21:17:43 +0300

    AssetProvider

---
+++ AssetProvider.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
+
+namespace AssetManagement
+{
+    public class AssetProvider : IAssetProvider
+    {
+        private readonly Dictionary<string, AsyncOperationHandle> _cache = new();
+        private readonly Dictionary<string, List<AsyncOperationHandle>> _usedResources = new();
+
+        public void Initialize()
+        {
+            Addressables.InitializeAsync();
+        }
+
+        public async Task<T> LoadAsync<T>(string address) where T : class
+        {
+            if (_cache.TryGetValue(address, out var completedHandle))
+            {
+                return (T) completedHandle.Result;
+            }
+            var handle = Addressables.LoadAssetAsync<T>(address);
+            RegisterCacheAndCleanup(address, handle);
+            return await handle.Task;
+        }
+        
+        public async Task<T> LoadAsync<T>(AssetReference assetReference) where T : class
+        {
+            if (_cache.TryGetValue(assetReference.AssetGUID, out var completedHandle))
+            {
+                return (T) completedHandle.Result;
+            }
+            var handle = Addressables.LoadAssetAsync<T>(assetReference);
+            RegisterCacheAndCleanup(assetReference.AssetGUID, handle);
+            return await handle.Task;
+        }
+        
+        public async Task<SceneInstance> LoadSceneAsync(string address)
+        {
+            if (_cache.TryGetValue(address, out var completedHandle))
+            {
+                return (SceneInstance) completedHandle.Result;
+            }
+            var handle = Addressables.LoadSceneAsync(address);
+            RegisterCacheAndCleanup(address, handle);
+            return await handle.Task;
+        }
+
+        public void Cleanup()
+        {
+            foreach (var resourceHandles in _usedResources.Values)
+            {
+                foreach (var handle in resourceHandles)
+                {
+                    Addressables.Release(handle);
+                }
+            }
+            _cache.Clear();
+            _usedResources.Clear();
+        }
+
+        private void RegisterCacheAndCleanup<T>(string key, AsyncOperationHandle<T> handle)
+        {
+            handle.Completed += completeHandle =>
+            {
+                _cache[key] = completeHandle;
+            };
+            RegisterForCleanup(key, handle);
+        }
+
+        private void RegisterForCleanup<T>(string guid, AsyncOperationHandle<T> handle)
+        {
+            if (!_usedResources.TryGetValue(guid, out var resourceHandles))
+            {
+                resourceHandles = new List<AsyncOperationHandle>();
+                _usedResources[guid] = resourceHandles;
+            }
+            resourceHandles.Add(handle);
+        }
+    }
+}
---
+++ IAssetProvider.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.ResourceProviders;
+
+namespace AssetManagement
+{
+    public interface IAssetProvider
+    {
+        void Initialize();
+        Task<T> LoadAsync<T>(string address) where T : class;
+        Task<T> LoadAsync<T>(AssetReference assetReference) where T : class;
+        Task<SceneInstance> LoadSceneAsync(string address);
+        void Cleanup();
+    }
+}
